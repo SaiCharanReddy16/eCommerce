acb46066acef07f3d446aa3720aa928829df3215
SaiCharanReddy16 <bushireddyscr@gmail.com>
2024-10-14 20:14:43 -0500

    testing the git commit from personal PC

--- sb-ecom/src/main/java/com/ecommerce/project/SbEcomApplication.java
+++ sb-ecom/src/main/java/com/ecommerce/project/SbEcomApplication.java
@@ -8,7 +8,7 @@ public class SbEcomApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hello world 1");
-		System.out.println("changed it in git IDE");
+		System.out.println("changed it in my personal pc");
 		SpringApplication.run(SbEcomApplication.class, args);
 	}
 
